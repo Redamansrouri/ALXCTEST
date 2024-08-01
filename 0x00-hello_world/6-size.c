22d2010535e26b7dc4590fa60e999b7408c626b3
Redamansrouri <mnreda58@gmail.com>
2024-08-01 18:58:49 +0100

    (:

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Size of a char: %lu\n", sizeof(char));
+    printf("Size of an int: %lu\n", sizeof(int));
+    printf("Size of a long int: %lu\n", sizeof(long int));
+    printf("Size of a long long int\n: %lu", sizeof(long long int));
+    printf("Size of a float: %lu", sizeof(float));
+}
